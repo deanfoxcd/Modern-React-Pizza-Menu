1ac8a2d6cd7f0642d46f480cc0920140634e4568
Dean Fox <deanfoxcd@gmail.com>
2024-11-10 20:00:59 -0500

    cleaned up files

--- src/index.js
+++ src/index.js
@@ -93,18 +93,6 @@ const Menu = () => {
 };
 
 const Pizza = ({ pizzaObj }) => {
-  // if (pizzaObj.soldOut)
-  //   return (
-  //     <li className="pizza sold-out">
-  //       <img src={pizzaObj.photoName} alt={pizzaObj.name}></img>
-  //       <div>
-  //         <h3>{pizzaObj.name}</h3>
-  //         <p>{pizzaObj.ingredients}</p>
-  //         <span>SOLD OUT</span>
-  //       </div>
-  //     </li>
-  //   );
-
   return (
     <li className={`pizza ${pizzaObj.soldOut ? 'sold-out' : ''}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name}></img>
